7827e6d992e83532bfe5317b32fe5c2523011913
jafet <jafet.manauth@Hotmail.com>
2021-07-30 11:25:38 -0500

    completed api for finding category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      attributes: ['category_name'],
+      //attributes: ['category_name'],
       include: [{model: Product, attributes:['product_name']}]
     });
     res.status(200).json(categoryData);
@@ -17,9 +17,18 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryId = await Category.findByPk(req.params.id, {
+      attributes: ['category_name'],
+      include: [{model:Product, attributes:['product_name']}]
+    });
+    res.status(200).json(categoryId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
