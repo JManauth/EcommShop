244bb712a019417565d72bb4106769e9e98f7f78
jafet <jafet.manauth@Hotmail.com>
2021-07-30 13:04:39 -0500

    added delete function to category routes, compelted category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,12 +41,38 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update({ category_name: req.body.new_name }, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No Category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
