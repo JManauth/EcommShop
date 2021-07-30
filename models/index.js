2fe50a9f5772744d410ba8ff45210af3e2b530b8
jafet <jafet.manauth@Hotmail.com>
2021-07-30 03:06:59 -0500

    completed seeds and app is able to seed on command

--- models/index.js
+++ models/index.js
@@ -5,14 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+ProductTag.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    //unique: true,
+  },
+ as: 'productTag'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(ProductTag, {
+  through: {
+    model: ProductTag,
+    //unique: true,
+  },
+  as: 'productTag'
+});
 module.exports = {
   Product,
   Category,
